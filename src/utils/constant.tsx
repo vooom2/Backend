69fc20d2413999269bf06dabaef07110d89eef75
Henry Chida <123965349+techchida@users.noreply.github.com>
2025-01-02 21:56:30 +0100

    initial commit

--- src/utils/constant.tsx
+++
@@ -1,5 +0,0 @@
-export const APP_NAME = "Vooom";
-export const API_BASE_URL = "https://api.voom.com";
-export const DEFAULT_LANGUAGE = "en";
-export const SUPPORT_EMAIL = "support@vooomshadcn.com";
-export const COPYRIGHT_YEAR = new Date().getFullYear().toString();
